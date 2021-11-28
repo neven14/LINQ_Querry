1f70e54a1b8763c19f10760b04a4bc0b179967aa
Neven <58472164+neven14@users.noreply.github.com>
2021-11-28 18:57:53 +0100

    Add files via upload

---
+++ LINQ_Querry543/LINQ_Querry543/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class Program
+{
+	public static void Main()
+	{
+		// Student collection
+		List<Student> studentList = new List<Student>() {
+				new Student() { StudentID = 1, StudentName = "John" } ,
+				new Student() { StudentID = 2, StudentName = "Moin" } ,
+				new Student() { StudentID = 3, StudentName = "Bill" } ,
+				new Student() { StudentID = 4, StudentName = "Ram" } ,
+				new Student() { StudentID = 5, StudentName = "Ron"  }
+			};
+
+		var res = from StudentName in studentList
+				  where studentList.Contains("B")
+				  select B;
+		foreach (var q in res)
+		{
+			Console.WriteLine(q);
+		}
+	}
+}
+
+
+public class Student
+{
+
+	public int StudentID { get; set; }
+	public string StudentName { get; set; }
+}
